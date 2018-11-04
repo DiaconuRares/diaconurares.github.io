51c73a816abea8be9f6ff8f0bac24bb07ee2c11c
DiaconuRares <40871918+DiaconuRares@users.noreply.github.com>
2018-11-04 18:49:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    $(".contMain>.container0").click(function() {
+        window.location.href = "https://diaconurares.github.io/WCP/index.html";
+    });
+    
+    $(".contMain>.container1").click(function() {
+        window.location.href = "https://diaconurares.github.io/Progressive/index.html";
+    });
+    
+    $(".contMain>.container2").click(function() {
+        window.location.href = "https://diaconurares.github.io/Simplist/index.html";
+    });
+    
+    $(".contMain>.container3").click(function() {
+        window.location.href = "https://diaconurares.github.io/progress/index.html";
+    });
+    
+});
